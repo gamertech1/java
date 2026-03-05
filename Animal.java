e889ba38b978ce5f4cdb31b7a1c140bf561834e4
gamertech <init.defaultBranch>
2026-03-05 19:04:14 +0530

    testing

--- Animal.java
+++ Animal.java
@@ -1,23 +1,26 @@
-public class Animal implements Comparable<Animal>{
+
+public class Animal implements Comparable<Animal> {
+
     int age;
     String name;
     int weight;
 
-    public Animal(int age, String name, int weight) {
-           this.age=age;
-           this.name=name;
-           this.weight=weight;
+    public Animal(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
+
     @Override
-    public String toString(){
-        return "Animal = "+"Age="+ age + " " +"Name="+ name +" " +"Weight="+ weight +"\n" ;
-        }
+    public String toString() {
+        return "Animal = " + "Age=" + age + " " + "Name=" + name + " ";
+    }
+
     @Override
-    public int compareTo(Animal a1){
-        if (this.age==a1.age) {
+    public int compareTo(Animal a1) {
+        if (this.age == a1.age) {
             return this.name.compareTo(a1.name);
         }
-       return this.age-a1.age;
-    }     
-    
+        return this.age - a1.age;
+    }
+
 }
